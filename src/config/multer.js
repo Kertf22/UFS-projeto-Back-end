6dae746b0c5ffd5ca8b2889a248cef51ef83a41c
Paulo Hercílio Araújo Santana <70054149+Kertf22@users.noreply.github.com>
2021-10-31 16:34:28 -0300

    Update multer.js

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,6 +1,7 @@
 const AWS = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
+require("dotenv/config")
 
 AWS.config.update({
     secretAccessKey: process.env.AWS_SECRET_KEY,
